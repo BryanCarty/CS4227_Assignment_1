ac2f027d01aeedcaa2db7d78348dca9a37c71c66
bcart <19235836@studentmail.ul.ie>
2023-02-11 22:37:32 +0000

    Martin Fowler Movie Rental Implementation

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        Movie HarryPotter = new Movie("Harry Potter", 0);
+        Rental harryRental = new Rental(HarryPotter, 3);
+        Customer johnny = new Customer("Johnny");
+        johnny.addRental(harryRental);
+        System.out.println(johnny.statement());
+        System.out.println(johnny.htmlStatement());
+    }
+}
+/**public class Main {
+    public static void main(String[] args){
+        Movie HarryPotter = new Movie("Harry Potter", 0);
+        Rental harryRental = new Rental(HarryPotter, 3);
+        Customer johnny = new Customer("Johnny");
+        johnny.addRental(harryRental);
+        System.out.println(johnny.statement());
+        System.out.println(johnny.htmlStatement());
+    }
+}
+**/
+
